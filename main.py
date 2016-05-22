47c21de7fcc24d8621b7bf5823ccbaa2f87e488e
rcuevass <rogercuevas.ca@gmail.com>
2016-05-21 23:59:01 -0400

    updating main program

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # Module that uses EXCLUSIVELY openCV
 import from_openCV as fOCV
-# Module written from scratch that generates gaussian pyramid
-import gauss_pyramid as gaussPy
+
+# gauss_pyramid is a module written from scratch that generates gaussian pyramid
+# only reduceImageSequence is invoked by main program
+from gauss_pyramid import reduceImageSequence
+
+# laplace_pyramid is a module written from scratch that generates gaussian pyramid
+# only ImagesLaplPyramid is invoked by main program
+from laplace_pyramid import ImagesLaplPyramid 
 
 
 def main():
@@ -10,7 +16,10 @@ def main():
 	fOCV.makeSequenceBlends('apple.jpg','orange.jpg',8)
 
 	# Generates gaussian pyramid using a module written from scratch
-	gaussPy.reduceImageSequence('apple.jpg',7)
+	reduceImageSequence('apple.jpg',7)
+
+	# Generates laplacian pyramid using a module written from scratch
+	ImagesLaplPyramid('apple.jpg',4)
 
 if __name__ == '__main__':
     main()
